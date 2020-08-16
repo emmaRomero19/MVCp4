4e3716db189a249d4bc7ee550b14ec1b8c08317b
Emma Romero <emmayrs17@gmail.com>
2020-08-15 23:10:53 -0500

    metodo busqueda

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ urls = (
     '/list', 'mvc.controllers.alumnos.list.Lista',
     '/insert', 'mvc.controllers.alumnos.insert.Insert',
     '/delete/?', 'mvc.controllers.alumnos.delete.Delete',
-    '/view/?', 'mvc.controllers.alumnos.viewOne.View',
+    '/view/(.*)', 'mvc.controllers.alumnos.viewOne.View',
     '/update', 'mvc.controllers.alumnos.update.Update',
 )
 app = web.application(urls, globals())
--- mvc/model/model.py
+++ mvc/model/model.py
@@ -41,9 +41,37 @@ class Alumnos():
             print("Error Select: ", e)
             result =[]
             return result
+    
+    def view(self, id_persona):
+        try:
+            self.connect()
+            query=('SELECT * FROM alumnos where id_alumno = %s;')
+            val_tupla=(id_persona,)
+            self.cursor.execute(query, val_tupla)
+            result=[]
+            for row in self.cursor:
+                r = {
+                    'id_persona' : row[0],
+                    'matricula' : row[1],
+                    'nombre' : row[2],
+                    'ap_paterno' : row[3],
+                    'ap_materno' : row[4],
+                    'edad' : row[5],
+                    'fecha_nac' : row[6],
+                    'genero' : row[7],
+                    'estado' : row[8],
+                    }
+                result.append(r)
+            self.cursor.close()
+            self.cnx.close()
+            return result
+        except Exception as e:
+            print("Error View: ", e)
+            result=[]
+            return result
 
 objeto=Alumnos()
 objeto.connect()
 
-for row in objeto.select():
+for row in objeto.view(2):
     print(row)
